5d52b9edfa726d3cf576126f954128a30abad0d7
bost-ty <61333914+bost-ty@users.noreply.github.com>
2024-09-30 18:03:59 -0700

    Improve measurement feedbacks

--- trapezoid.js
+++ trapezoid.js
@@ -3,9 +3,11 @@ const btnCalc = document.getElementById("calculate");
 btnCalc.addEventListener("click", function () {
 	const info = document.getElementById("info");
 	const output = document.getElementById("output");
-	const long = document.getElementById("long-base").value;
-	const short = document.getElementById("short-base").value;
-	const height = document.getElementById("height").value;
+	const long = Number.parseFloat(document.getElementById("long-base").value);
+	const short = Number.parseFloat(
+		document.getElementById("short-base").value
+	);
+	const height = Number.parseFloat(document.getElementById("height").value);
 
 	let newBase = (long - short) / 2;
 	let legLength = Math.sqrt(Math.pow(newBase, 2) + Math.pow(height, 2));
@@ -14,9 +16,13 @@ btnCalc.addEventListener("click", function () {
 	}
 
 	if (newBase == height && long != 0 && short != 0 && height != 0) {
-		info.textContent = `This will have a perfect 45 degree angle on each leg.`;
+		info.textContent = `These measurements will have a perfect 45 degree angle for each leg.`;
 	} else if (newBase != height && long != 0 && short != 0 && height != 0) {
-		info.textContent = `This will not have a perfect 45 degree angle on each leg. To achieve this, (long base - short base / 2) must be equal to your desired height.`;
+		info.textContent = `For a perfect 45 degree angle for each leg, your desired height would need to be ${
+			(long - short) / 2
+		}. If that's the height you want, your long side should be ${
+			2 * height + short
+		}.`;
 	} else if (long == 0 || short == 0 || height == 0) {
 		info.textContent = `Please fill out all three fields above.`;
 	}
