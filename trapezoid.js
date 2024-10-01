b61b84f0f069e937292ad279ac5c75cd8a683f5c
bost-ty <61333914+bost-ty@users.noreply.github.com>
2024-09-30 17:58:02 -0700

    Get values onclick

--- trapezoid.js
+++ trapezoid.js
@@ -1,26 +1,23 @@
 const btnCalc = document.getElementById("calculate");
+
 btnCalc.addEventListener("click", function () {
-  const info = document.getElementById("info");
-  const output = document.getElementById("output");
-  const long = document.getElementById("long-base");
-  const short = document.getElementById("short-base");
-  const height = document.getElementById("height");
+	const info = document.getElementById("info");
+	const output = document.getElementById("output");
+	const long = document.getElementById("long-base").value;
+	const short = document.getElementById("short-base").value;
+	const height = document.getElementById("height").value;
 
-  long = Number.parseFloat(long.value);
-  short = Number.parseFloat(short.value);
-  height = Number.parseFloat(height.value);
+	let newBase = (long - short) / 2;
+	let legLength = Math.sqrt(Math.pow(newBase, 2) + Math.pow(height, 2));
+	if (legLength != 0) {
+		output.value = legLength;
+	}
 
-  let newBase = (long - short) / 2;
-  let legLength = Math.sqrt(Math.pow(newBase, 2) + Math.pow(height, 2));
-  if (legLength != 0) {
-    output.value = legLength;
-  }
-
-  if (newBase == height && long != 0 && short != 0 && height != 0) {
-    info.textContent = `This will have a perfect 45 degree angle on each leg.`;
-  } else if (newBase != height && long != 0 && short != 0 && height != 0) {
-    info.textContent = `This will not have a perfect 45 degree angle on each leg. To achieve this, (long base - short base / 2) must be equal to your desired height.`;
-  } else if (long == 0 || short == 0 || height == 0) {
-    info.textContent = `Please fill out all three fields above.`;
-  }
+	if (newBase == height && long != 0 && short != 0 && height != 0) {
+		info.textContent = `This will have a perfect 45 degree angle on each leg.`;
+	} else if (newBase != height && long != 0 && short != 0 && height != 0) {
+		info.textContent = `This will not have a perfect 45 degree angle on each leg. To achieve this, (long base - short base / 2) must be equal to your desired height.`;
+	} else if (long == 0 || short == 0 || height == 0) {
+		info.textContent = `Please fill out all three fields above.`;
+	}
 });
