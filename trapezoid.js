113ca1a0f5041ae9985c4bf3cf5d4751d5e0ea5c
bost-ty <61333914+bost-ty@users.noreply.github.com>
2024-09-30 18:08:14 -0700

    Clean up formula representation, improve error handling

--- trapezoid.js
+++ trapezoid.js
@@ -11,13 +11,19 @@ btnCalc.addEventListener("click", function () {
 
 	let newBase = (long - short) / 2;
 	let legLength = Math.sqrt(Math.pow(newBase, 2) + Math.pow(height, 2));
-	if (legLength != 0) {
+	if (legLength != 0 && legLength) {
 		output.value = legLength;
 	}
 
 	if (newBase == height && long != 0 && short != 0 && height != 0) {
 		info.textContent = `These measurements will have a perfect 45 degree angle for each leg.`;
-	} else if (newBase != height && long != 0 && short != 0 && height != 0) {
+	} else if (
+		newBase != height &&
+		long != 0 &&
+		short != 0 &&
+		height != 0 &&
+		legLength
+	) {
 		info.textContent = `For a perfect 45 degree angle for each leg, your desired height would need to be ${
 			(long - short) / 2
 		}. If that's the height you want, your long side should be ${
